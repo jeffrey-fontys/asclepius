c26774eb34f91d64c7f255f96e6652fef062abfd
Jeffrey Derksen <74195106+jeffrey-fontys@users.noreply.github.com>
2023-03-20 20:17:54 +0100

    Added case 1 yarn script

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -6,6 +6,7 @@ public class ItemPickup : MonoBehaviour
 {
     public string Name;
     public string Description;
+    public TreatmentStep TreatmentStep;
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class PlayerController : MonoBehaviour
 {
     public TreatmentPlan TreatmentPlan;
-    public GameObject HeldItem;
+    public ItemPickup HeldItem;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +20,24 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public void PickupItem(GameObject gameObject)
+    public void PickupItem(ItemPickup item)
     {
-        if (HeldItem != null) HeldItem.SetActive(true);
+        if (HeldItem != null) HeldItem.gameObject.SetActive(true);
 
-        HeldItem = gameObject;
-        HeldItem.SetActive(false);
+        HeldItem = item;
+        HeldItem.gameObject.SetActive(false);
     }
 
-    public void UseItem()
+    [YarnCommand("use_held_item")]
+    public void UseHeldItem()
     {
         if (!HeldItem) return;
+        TreatmentPlan.TreatmentAction(HeldItem.TreatmentStep);
     }
+
+    //public static bool HasItem(string name)
+    //{
+    //    if (HeldItem != null && HeldItem.Name == name) return true;
+    //    else return false;
+    //}
 }
