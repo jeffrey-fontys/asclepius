85b587e6069c20149edcf0e323dc15dfe4f325ce
Jeffrey Derksen <74195106+jeffrey-fontys@users.noreply.github.com>
2023-04-19 14:01:56 +0200

    Saving and loading of player settings

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -8,19 +10,39 @@ public class SettingsMenu : MonoBehaviour
     public ContinuousTurnProviderBase ContinuousTurnProvider;
     public SnapTurnProviderBase SnapTurnProvider;
 
+    public Slider MainVolumeSlider;
+    public Slider MusicVolumeSlider;
+    public Slider SFXVolumeSlider;
+    public TMP_Dropdown TurnProviderDropdown;
+
+    private void Start()
+    {
+        MainVolumeSlider.value = PlayerPrefs.GetFloat("VolumeMain", 0);
+        MusicVolumeSlider.value = PlayerPrefs.GetFloat("VolumeMusic", 0);
+        SFXVolumeSlider.value = PlayerPrefs.GetFloat("VolumeSFX", 0);
+        TurnProviderDropdown.value = PlayerPrefs.GetInt("TurnProvider", 0);
+        TurnProviderDropdown.RefreshShownValue();
+    }
+
     public void SetVolumeMain(float volume)
     {
         AudioMixer.SetFloat("VolumeMain", volume);
+        PlayerPrefs.SetFloat("VolumeMain", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetVolumeMusic(float volume)
     {
         AudioMixer.SetFloat("VolumeMusic", volume);
+        PlayerPrefs.SetFloat("VolumeMusic", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetVolumeSFX(float volume)
     {
         AudioMixer.SetFloat("VolumeSFX", volume);
+        PlayerPrefs.SetFloat("VolumeSFX", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetTurnProvider(int turnIndex)
@@ -36,5 +58,7 @@ public class SettingsMenu : MonoBehaviour
                 SnapTurnProvider.enabled = false;
                 break;
         }
+        PlayerPrefs.SetInt("TurnProvider", turnIndex);
+        PlayerPrefs.Save();
     }
 }
